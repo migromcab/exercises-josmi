f994a619a18d1c7ad49b1233d86ad3bda211d198
xuani <juasosua@gmail.com>
2023-11-28 20:49:32 +0100

    ranking caza al murciano

---
+++ caza-al-murciano/ranking.js
@@ -0,0 +1,14 @@
+const ranking = [
+
+    {position: 1, name: 'Pepito', scoring: 3}
+    {position: 2, name: 'Pepita', scoring: 2}
+    
+]
+
+const rankingNode = document.querySelector('.ranking');
+const htmlPosition = '< div class = "ranking__item">' '<div>' ' <strong>'  ${positionInfo.position}'</strong>1</strong>' ${positionInfo.name} '<div></div>'${positionInfo.scoring} ${pointsLabel}'<div></div>'
+ranking.forEach(positionInfo => {
+
+})
+
+rankingNode.innerHTML = rankingHtml
