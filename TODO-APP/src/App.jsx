947b2299df940fa61541bed967da96bcbe28ab9f
José <helloyinx@gmail.com>
2024-01-09 20:58:28 +0100

    react

--- TODO-APP/src/App.jsx
+++ TODO-APP/src/App.jsx
@@ -8,27 +8,39 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='topbar'>
+        <div className='button style'>
+          <a href="">All</a>
+          <a href="">Completed</a>
+          <a href="">Pending</a>
+        </div>
+
+       
       </div>
-      <h1>Vite + React</h1>
-      <h2>Hola mundo</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className='button space style'>
+    
+        <input type="text" placeholder='Escribe algo'/>
+        <a href="">Add</a>
+        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div className=' '>
+        <div className='style button tasks'>
+          <input type="checkbox" />
+        <label htmlFor="">Tarea 1</label>
+        <a href="">Delete</a>
+
+        </div>
+       
+        <div className='style button tasks'>
+        <input type="checkbox" />
+        <label htmlFor="">Tarea 2</label>
+        <a href="">Delete</a>
+        </div>
+       
+      </div>
+
     </>
   )
 }
