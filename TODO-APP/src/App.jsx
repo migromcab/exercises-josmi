4f7d74fc4564e78b9ae164bc72628b65c47937c8
José <helloyinx@gmail.com>
2024-01-16 20:54:43 +0100

    react

--- TODO-APP/src/App.jsx
+++ TODO-APP/src/App.jsx
@@ -1,48 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
+  const tasks = [
+    {
+      id: 0,
+      text: "Tarea 1",
+      isCompleted: true,
+    },
+    {
+      id: 1,
+      text: "Tarea 2",
+      isCompleted: false,
+    },
+  ];
   return (
     <>
-      <div className='topbar'>
-        <div className='button style'>
+      <div className="topbar">
+        <div className="button style">
           <a href="">All</a>
           <a href="">Completed</a>
           <a href="">Pending</a>
         </div>
-
-       
       </div>
 
-      <div className='button space style'>
-    
-        <input type="text" placeholder='Escribe algo'/>
+      <div className="button space style">
+        <input type="text" placeholder="Escribe algo" />
         <a href="">Add</a>
-        
       </div>
 
-      <div className=' '>
-        <div className='style button tasks'>
+      <div className=" ">
+        <div className="style button tasks">
           <input type="checkbox" />
-        <label htmlFor="">Tarea 1</label>
-        <a href="">Delete</a>
+          <label htmlFor="">Tarea 1</label>
+          <a href="">Delete</a>
+        </div>
 
+        <div className="style button tasks">
+          <input type="checkbox" />
+          <label htmlFor="">Tarea 2</label>
+          <a href="">Delete</a>
         </div>
-       
-        <div className='style button tasks'>
-        <input type="checkbox" />
-        <label htmlFor="">Tarea 2</label>
-        <a href="">Delete</a>
-        </div>
-       
+
+        {tasks.map((task, index) => (
+          <Task key={index} text={task.text} isCompleted={task.isCompleted} />
+        ))}
       </div>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
